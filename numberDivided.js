e624dc9140be83efd606940cf3609d9040914708
julissa Bernal <152914827+JulissaBernal@users.noreply.github.com>
2024-10-31 11:57:56 -0600

    Number Divided finish

---
+++ numberDivided.js
@@ -0,0 +1,12 @@
+// Given a number, return the number divided into its halves in an array.
+// Examples:
+// - `numDiv(4)` should return `[2, 2]`.
+// - `numDiv(10)` should return `[5, 5]`.
+
+let number=10;
+let numDiv=[];
+
+numDiv[0]=number/2;
+numDiv[1]=number/2;
+
+console.log(numDiv);
